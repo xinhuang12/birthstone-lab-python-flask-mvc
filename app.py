bde005d21fdfc114ff739d2477ebf73b85f3dda0
Xin <xinhuang101@gmail.com>
2021-07-19 18:25:03 +0000

    xinhuang12

--- app.py
+++ app.py
@@ -15,3 +15,28 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/results', methods=["GET", "POST"])
+def results():
+    print(request.form['gemstone'])
+    user_choice=request.form['gemstone']
+    gemstone={
+        "January":"your birthstone is: Garnet",
+        "Febuary":"your birthstone is: Anethyst",
+        "March":"your birthstone is: Aquamarine",
+        "April":"your birthstone is: Diamond",
+        "May":"your birthstone is: Emerald",
+        "June":"your birthstone is: Pearl",
+        "July":"your birthstone is: Ruby",
+        "August":"your birthstone is: Peridot",
+        "September":"your birthstone is: Sapphire",
+        "October":"your birthstone is: Opal",
+        "November":"your birthstone is: Citrine",
+        "December":"your birthstone is: Topaz"}
+    try:
+        message=gemstone[user_choice]
+
+    except:
+        message=("error, please try another month")
+        
+    return render_template("results.html", message=message)
